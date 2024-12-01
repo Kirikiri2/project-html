8d41c3be49fd68da5c28ee78c7be7fea932023c5
Kirikiri2 <worklisa271@gmail.com>
2024-12-01 22:09:33 +0300

    слайдер без анимации

--- node.js
+++
@@ -1,7 +0,0 @@
-const sliderContainer = document.querySelector('.catalog-card');
-const rangeInput = document.querySelector('.slider-range');
-
-rangeInput.addEventListener('input', (event) => {
-  const value = parseInt(event.target.value);
-  sliderContainer.style.transform = `translateX(-${value * 20}%)`;
-});
