508d6c231031dfb5b73397bdccadaf71cfd571e6
Kirikiri2 <worklisa271@gmail.com>
2024-12-03 14:14:01 +0300

    слайдер + карточки

---
+++ node.js
@@ -0,0 +1,9 @@
+const sliderContainer = document.querySelector('.catalog-card');
+const rangeInput = document.querySelector('.slider-range');
+
+rangeInput.addEventListener('input', (event) => {
+  const value = parseInt(event.target.value);
+  const itemWidth = document.querySelector('.catalog-card-with-text').offsetWidth + 20; // Ширина + отступ
+  const offset = -(value * itemWidth);
+  sliderContainer.style.transform = `translateX(${offset}px)`;
+});
