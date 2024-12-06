6bbba1d3c350707e39af4f8b1d741b133a492619
Kirikiri2 <worklisa271@gmail.com>
2024-12-06 17:48:27 +0300

    горе почта

--- node.js
+++ node.js
@@ -7,9 +7,3 @@ rangeInput.addEventListener('input', (event) => {
   const offset = -(value * itemWidth);
   sliderContainer.style.transform = `translateX(${offset}px)`;
 });
-
-document.querySelectorAll(".clickable").forEach(link => {
-  link.addEventListener("click", () => {
-    document.getSelector("email-input").focus();
-  });
-});
